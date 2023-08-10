93d74a3473b7cd39af7dc49f3efab9d378e222b2
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-10 17:19:26 +0300

    add solution for homework for 09.10.2023

--- src/components/Categories/CategoryList.js
+++ src/components/Categories/CategoryList.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import CategoryCard from './CategoryCard';
+import React from "react";
+import CategoryCard from "./CategoryCard";
+import {Link} from "react-router-dom";
 
-const CategoryList = ({ categories }) => {
-  return (
-    <div className="category-list">
-        {/* add link to each of these */}
-        <CategoryCard id="potions" categoryData={categories[0]} />
-        <CategoryCard id="charms" categoryData={categories[1]} />
-
-      {categories.slice(2).map(category => (
-          <CategoryCard categoryData={category} />
-        ))}
-    </div>
-  );
-}
+const CategoryList = ({categories}) => {
+    return (
+        <div className = "category-list">
+            {categories.slice(0, 2).map(category => (
+                <Link to = {"/wiki/" + category.routeLink}><CategoryCard id = {category.id} categoryData = {category}/></Link>
+            ))}
+            {categories.slice(2).map(category => (
+                <CategoryCard id = {category.routeLink} categoryData = {category}/>
+            ))}
+        </div>
+    );
+};
 
 export default CategoryList;
--- src/components/Header/HogwartNavbar.js
+++ src/components/Header/HogwartNavbar.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './HogwartNavbar.css';
+import React from "react";
+import "./HogwartNavbar.css";
+import {Link} from "react-router-dom";
 
 const HogwartNavbar = () => {
-  return (
-    <nav className="navbar">
-        <div className="navbar-link">Home</div>
-        <div className="navbar-link">About</div>
-    </nav>
-  );
-}
-
+    return (
+        <nav className = "navbar">
+            <div className = "navbar-link"><Link to = "/">Home</Link></div>
+            <div className = "navbar-link"><Link to = "/about">About</Link></div>
+        </nav>
+    );
+};
 
 export default HogwartNavbar;
