0b8f76bc11e7c74ed9514686b47b2cf785cb18e8
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-15 18:32:28 +0300

    some refactor

--- src/components/Categories/CategoryList.js
+++ src/components/Categories/CategoryList.js
@@ -6,7 +6,9 @@ const CategoryList = ({categories}) => {
     return (
         <div className = "category-list">
             {categories.slice(0, 2).map(category => (
-                <Link to = {"/wiki/" + category.routeLink}><CategoryCard id = {category.id} categoryData = {category}/></Link>
+                <Link to = {"/wiki/" + category.routeLink}>
+                    <CategoryCard id = {category.id} categoryData = {category}/>
+                </Link>
             ))}
             {categories.slice(2).map(category => (
                 <CategoryCard id = {category.routeLink} categoryData = {category}/>
